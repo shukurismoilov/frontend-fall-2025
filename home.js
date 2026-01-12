327cffa79ee20d2217cbf134e97707301bc6a3a4
Shukur <shukur_007_96@mail.ru>
2026-01-12 16:55:33 +0500

    initial message

---
+++ home.js
@@ -0,0 +1,15 @@
+// Прочитать про методы массивов
+// .sort .filter .map .reduce .splice
+
+const users = [
+  {
+    id: "#1",
+    login: "shukur",
+    password: "1234567",
+  },
+  {
+    id: "#2",
+    login: "alex1990",
+    password: "strongandsecurepassword",
+  },
+];
